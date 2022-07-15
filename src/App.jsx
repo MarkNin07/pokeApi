debd4807490da9a79b76509bd2107127a0a3f2a5
Marcos Nino <tmnino@uninorte.edu.co>
2022-07-14 22:53:09 -0500

    Fetch Api
    Two functions were created (getAllPokemons & pokemonObject) in order to bring all of the information about the pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [allPokemons, setAllPokemons] = useState([])
+
+  const getAllPokemons = async () => {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+    const data = await res.json()
+
+    function pokemonObject (result){
+      result.forEach(async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+
+        setAllPokemons(currentList => [...currentList, data])
+      })
+    }
+    pokemonObject(data.results)
+    await console.log(allPokemons)
+
+  }
+
+  useEffect(() => {
+    getAllPokemons()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app-container">
+      <h1>Pokemons!</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+
+        </div>
+      </div>
     </div>
   )
 }
